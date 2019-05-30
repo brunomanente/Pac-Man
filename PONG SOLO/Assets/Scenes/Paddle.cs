62e506427a488dcb30c10a8c1bb8f857edf825bd
brunomanente <bruno.manente0@gmail.com>
2019-05-29 22:29:43 -0300

    PACMAN COMPLETO

---
+++ PONG SOLO/Assets/Scenes/Paddle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle)
+        {
+            pos = new Vector2(GameManager.topRight.x, 0);
+        }else
+        {
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void Init(bool v)
+    {
+        throw new NotImplementedException();
+    }
+}
